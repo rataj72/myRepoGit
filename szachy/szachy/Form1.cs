82f2a01cf58c95d22875a5909c4ccd1ca5576061
piotr-Komputer\piotr <pkubon2@gmail.com>
2017-08-09 02:41:02 +0200

    szachy v2

---
+++ szachy/szachy/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szachy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateButton();
+        }
+
+        public void CreateButton()
+        {
+            Button button = new Button();
+            Controls.Add(button);
+        }
+    }
+}
